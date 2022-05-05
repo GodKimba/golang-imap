76485308766bada9057c7ca361eccb4b869effbb
GodKimba <rafaelfadel2@gmail.com>
2022-05-05 15:44:32 -0300

    added the correct function to login in the gmail server

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
+      "log"
       "github.com/emersion/go-imap/client"
-      "github.com/emersion/go-imap"
 )
+      //"github.com/emersion/go-imap"
+
+const userMailAccount = "user@mail.acc"
+const mailServer = "imap.gmail.com:993"
+const userMailPassword = "password"
+
+func connectToClientServer() {
+  c, err := client.DialTLS(mailServer, nil)
+  if err != nil {
+    log.Fatalf("Couldn't login, %v", err)
+  }
+  log.Println("Connected")
+
+  defer c.Logout()
+
+  if err := c.Login(userMailAccount, userMailPassword); err != nil {
+    log.Fatalf("Couldn't login, %v", err)
+  }
+  log.Println("Logged in!")
+}
+
+func selectMailBox() {
+  connectToClientServer()
+  mbox, err := c.selectMailBox("INBOX", false)
+  if err != nil {
+    log.Fatal(err)
+  }
+  log.Println("Flags for inbox: ", mbox.Flags)
+
+}
 
 func main() {
-  
+  connectToClientServer()
 }
