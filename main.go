827b0e12a629b132f6e414a0fcdedb70ae96aab1
GodKimba <rafaelfadel2@gmail.com>
2022-05-07 10:53:02 -0300

    added more functionality to the cost of redability

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"github.com/emersion/go-imap/client"
 	"log"
-	"github.com/joho/godotenv"
 	"os"
-	"fmt"
+
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	"github.com/joho/godotenv"
 )
 
 //"github.com/emersion/go-imap"
@@ -14,45 +15,69 @@ import (
 // Function to get the environment variable from .evn
 func getEnvKey(key string) string {
 	err := godotenv.Load()
-	
+
 	if err != nil {
 		log.Fatalf("Error  loading .env file")
 	}
-	
+
 	return os.Getenv(key)
 }
 
 var userMailAccount = getEnvKey("USERNAME")
 var userMailPassword = getEnvKey("PASSWORD")
+
 const mailServer = "imap.gmail.com:993"
 
-
-
-func connectToClientServer() {
+func main() {
+	// Connecting to the server
 	c, err := client.DialTLS(mailServer, nil)
 	if err != nil {
 		log.Fatalf("Couldn't login, %v", err)
 	}
-	
 
 	log.Println("Connected")
+
 	defer c.Logout()
+
+	// Login in the user account
 	if err := c.Login(userMailAccount, userMailPassword); err != nil {
 		log.Fatalf("Couldn't login, %v", err)
 	}
 	log.Println("Logged in!")
-}
 
-//func selectMailBox() {
-	//mbox, err := c.Select("INBOX", false)
-	//if err != nil {
-		//log.Fatal(err)
-	//}
-	//log.Println("Flags for inbox: ", mbox.Flags)
+	// Selecting the flags of the inbox
+	mbox, err := c.Select("INBOX", false)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Flags for inbox: ", mbox.Flags)
 
-//}
+	// Selecting the Search criteria
+	criteria := imap.NewSearchCriteria()
+	criteria.WithoutFlags = []string{imap.SeenFlag}
+	ids, err := c.Search(criteria)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("IDs found:", ids)
 
-func main() {
-	fmt.Println(userMailAccount, userMailPassword)
-	connectToClientServer()
+	if len(ids) > 0 {
+		seqset := new(imap.SeqSet)
+		seqset.AddNum(ids...)
+
+		messages := make(chan *imap.Message, 10)
+		done := make(chan error, 1)
+		go func() {
+			done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+		}()
+
+		log.Println("Unseen messages:")
+		for msg := range messages {
+			log.Println("* " + msg.Envelope.Subject)
+		}
+
+		if err := <-done; err != nil {
+			log.Fatal(err)
+		}
+	}
 }
