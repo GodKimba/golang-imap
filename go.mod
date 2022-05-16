95333906093ccccd451a4e6b36697bc1d5e77d8c
GodKimba <rafaelfadel2@gmail.com>
2022-05-16 14:30:58 -0300

    got the search criteria function working

--- go.mod
+++ go.mod
@@ -2,10 +2,12 @@ module github.com/GodKimba/golang-imap
 
 go 1.18
 
-require github.com/emersion/go-imap v1.2.1
+require (
+	github.com/emersion/go-imap v1.2.1
+	github.com/joho/godotenv v1.4.0
+)
 
 require (
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
-	github.com/joho/godotenv v1.4.0 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/emersion/go-imap"
 	"github.com/emersion/go-imap/client"
 	"github.com/joho/godotenv"
-	"ioutil"
-	"log"
-	"os"
 )
 
 //"github.com/emersion/go-imap"
@@ -19,19 +15,20 @@ import (
 // Function to get the environment variable from .evn
 func getEnvKey(key string) string {
 	err := godotenv.Load()
-	
+
 	if err != nil {
 		log.Fatalf("Error  loading .env file")
 	}
-	
+
 	return os.Getenv(key)
 }
 
 var userMailAccount = getEnvKey("USERNAME")
 var userMailPassword = getEnvKey("PASSWORD")
-const mailServer = "imap.gmail.com:993"
-var c *client.Client
 
+const mailServer = "imap.gmail.com:993"
+
+var c *client.Client
 
 func connectToClientServer(c *client.Client, err error) {
 	c, err = client.DialTLS(mailServer, nil)
@@ -39,10 +36,9 @@ func connectToClientServer(c *client.Client, err error) {
 		log.Fatalf("Couldn't login, %v", err)
 	}
 
-	
 	log.Println("Connected")
 	defer c.Logout()
-	
+
 	if err := c.Login(userMailAccount, userMailPassword); err != nil {
 		log.Fatalf("Couldn't login, %v", err)
 	}
@@ -53,71 +49,42 @@ func connectToClientServer(c *client.Client, err error) {
 		log.Fatal(err)
 	}
 	log.Println("Flags for inbox: ", mbox.Flags)
-	
+
 	// Searching criteria almost working, lines below are responsible for it
-	keyword := "rafael"
 	criteria := imap.NewSearchCriteria()
-	criteria.Header.Get(keyword)
-	c.Search(criteria)
-	fmt.Println(criteria)
-	
+	criteria.Header.Add("SUBJECT", "Rafael")
+	ids, err := c.Search(criteria)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("IDs found:", ids)
+
+	if len(ids) > 0 {
+		seqset := new(imap.SeqSet)
+		seqset.AddNum(ids...)
+
+		messages := make(chan *imap.Message, 10)
+		done := make(chan error, 1)
+		go func() {
+			done <- c.Fetch(seqset, []imap.FetchItem{imap.FetchEnvelope}, messages)
+		}()
+
+		log.Println("Unseen messages:")
+		for msg := range messages {
+			log.Println("* " + msg.Envelope.Subject)
+		}
+
+		if err := <-done; err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+func main() {
+	connectToClientServer(c, nil)
 }
 
 //func selectMailBox(c *client.Client, err error) {
 //}
-
-func main() {
-	fmt.Println(userMailAccount, userMailPassword)
-	connectToClientServer(c, nil)
-
-
-	
-}
-
-	
-
-	if mbox.Messages == 0 {
-		log.Fatal("No message in mailbox")
-	}
-	seqset := new(imap.SeqSet)
-	seqset.AddRange(mbox.Messages, mbox.Messages)
-	section := &imap.BodySectionName{}
-	items := []imap.FetchItem{section.FetchItem()}
-
-	messages := make(chan *imap.Message, 1)
-	done := make(chan error, 1)
-	go func() {
-		done <- c.Fetch(seqset, items, messages)
-	}()
-
-	log.Println("Last message:")
-	msg := <-messages
-	r := msg.GetBody(section)
-	if r == nil {
-		log.Fatal("Server didn't returned message body")
-	}
-
-	if err := <-done; err != nil {
-		log.Fatal(err)
-	}
-
-	m, err := mail.ReadMessage(r)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	header := m.Header
-	log.Println("Date:", header.Get("Date"))
-	log.Println("From:", header.Get("From"))
-	log.Println("To:", header.Get("To"))
-	log.Println("Subject:", header.Get("Subject"))
-
-	body, err := ioutil.ReadAll(m.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(body)
-}
 
 // Expunge func
 // item := imap.FormatFlagsOp(imap.AddFlags, true)
@@ -125,10 +92,8 @@ func main() {
 // if err := c.Store(seqset, item, flags, nil); err != nil {
 // 	log.Fatal(err)
 // }
-
 // // Then delete it
 // if err := c.Expunge(nil); err != nil {
 // 	log.Fatal(err)
 // }
-
 // log.Println("Last message has been deleted")
